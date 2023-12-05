220fb6b7117775aa5861f324250a2ded02b1cbc9
ken-watarai <66110193+ken-watarai@users.noreply.github.com>
2023-12-05 19:34:37 +0900

    Add files via upload

--- script.js
+++ script.js
@@ -1,3 +1,52 @@
+const section1_comment = document.getElementById("section1-comment");
+const section1_comments = [
+  "ごろごろ大好き",
+  "だって布団がまだって言うから",
+  "ごろごろは平和",
+  "平和が一番",
+  "布団は平和の象徴",
+  "鳩じゃない、鳩は許さないわよ",
+  "ところで今日何日？",
+  "あ、誕生日だ、",
+  "あぶない、<br>寝過ごすとこだった",
+  "わてぃに祝ってもらおうかしら",
+  "かしこまりました！wty"
+]
+
+const section2_comment = document.getElementById("section2-comment");
+const section2_comments = [
+  "どうでしょうかwty",
+  "まあ点数はLINEするね",
+  "わてぃ、これいつ作ってたの？",
+  "夜中ですwty",
+  "今眠い？",
+  "はいwty",
+  "わたしも",
+  "知ってるwty",
+  "zzz",
+  "寝るのはやっ、wty"
+]
+
+function comment_generator(list, sec_cmt) {
+  for (let i = 0; i < list.length; i++) {
+    const newP = document.createElement("p");
+    const newSpan = document.createElement("span");
+    let comment = list[i];
+    if(comment.slice(-3) == "wty"){
+      const wty_cmt = comment.replace("wty", "");
+      newSpan.textContent = wty_cmt;
+      newP.classList.add('watty');
+    } else {
+      newSpan.textContent = comment;
+    }
+    newP.appendChild(newSpan);
+    sec_cmt.appendChild(newP);
+  }
+}
+comment_generator(section1_comments, section1_comment);
+comment_generator(section2_comments, section2_comment);
+
+
 window.addEventListener("load", function(){
 
     //プラグインを定義
